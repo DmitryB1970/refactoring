027bd808227335daf9dd02f64e62b5cfe2e64a2a
DmitryB1970 <bdmitrij121@gmail.com>
2024-10-20 13:30:04 +0300

    made corrections

--- src/main/java/com/javaacademy/GarbageNotRefactorableException.java
+++ src/main/java/com/javaacademy/GarbageNotRefactorableException.java
@@ -2,23 +2,24 @@ package com.javaacademy;
 
 
 import lombok.NoArgsConstructor;
+import lombok.experimental.StandardException;
 
-@NoArgsConstructor
+@StandardException
 public class GarbageNotRefactorableException extends Exception {
 
-    public GarbageNotRefactorableException(String message) {
-        super(message);
-    }
-
-    public GarbageNotRefactorableException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public GarbageNotRefactorableException(Throwable cause) {
-        super(cause);
-    }
-
-    public GarbageNotRefactorableException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
+//    public GarbageNotRefactorableException(String message) {
+//        super(message);
+//    }
+//
+//    public GarbageNotRefactorableException(String message, Throwable cause) {
+//        super(message, cause);
+//    }
+//
+//    public GarbageNotRefactorableException(Throwable cause) {
+//        super(cause);
+//    }
+//
+//    public GarbageNotRefactorableException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+//        super(message, cause, enableSuppression, writableStackTrace);
+//    }
 }
--- src/main/java/com/javaacademy/UtilizationFactory.java
+++ src/main/java/com/javaacademy/UtilizationFactory.java
@@ -1,6 +1,7 @@
 package com.javaacademy;
 
 import lombok.NoArgsConstructor;
+import lombok.experimental.UtilityClass;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -9,25 +10,25 @@ import java.io.IOException;
  * Фабрика по переработке мусора
  */
 
-@NoArgsConstructor
+@UtilityClass
 public class UtilizationFactory {
     public static final double BOTTLE_SIZE = 500;
 
-    private static Bottle refactorGlassGarbage(Garbage garbage) throws GarbageNotRefactorableException {
+    private Bottle refactorGlassGarbage(Garbage garbage) throws GarbageNotRefactorableException {
         if (garbage.getGarbageType() != GarbageType.GLASS) {
             throw new GarbageNotRefactorableException("Мусор не состоит полностью из стекла!");
         }
         return new Bottle(BOTTLE_SIZE, garbage.getCityFrom());
     }
 
-    private static Cartoon refactorPaperGarbage(Garbage garbage) throws GarbageNotRefactorableException {
+    private Cartoon refactorPaperGarbage(Garbage garbage) throws GarbageNotRefactorableException {
         if (garbage.getGarbageType() != GarbageType.PAPER) {
             throw new GarbageNotRefactorableException("Мусор не состоит полностью из бумаги!");
         }
         return new Cartoon(garbage.getWeight() / 2);
     }
 
-    public static void refactorGarbage(Garbage[] garbageArray, BufferedWriter journal)
+    public void refactorGarbage(Garbage[] garbageArray, BufferedWriter journal)
             throws GarbageNotRefactorableException, IOException {
         for (Garbage garbage : garbageArray) {
             switch (garbage.getGarbageType()) {
